8ed6632c56d51b51bf4752bd25dd8c1631795d3a
Kai Clausen <kaiclausen123@gmail.com>
2023-01-09 13:29:36 -0800

    Create an explanation of the result in the window alert.

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,10 @@ function multiply(number1, number2) {
 }
 
 function divide(number1, number2) {
-  return number1 / number 2;
+  return number1 / number2;
 }
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+window.alert("The result of the division of " + number1 + " and " + number2 + " is: " + divide(number1, number2)) 
