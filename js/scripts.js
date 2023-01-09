5a8c6d2ffb6e2455a201a9ffeb4595fde9b8c61a
Kai Clausen <kaiclausen123@gmail.com>
2023-01-09 12:03:00 -0800

    add business and ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
 function add(number1, number2) {
   return number1 + number2;
 }
 
-saySomething("The sum is " + add(3,5) + ".")
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+window.alert(add(number1, number2));
